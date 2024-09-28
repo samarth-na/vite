ef8f8e4afe2de358fbe23efd75a0d1fed7f1a713
Samarth Nagar <samarthxma2009@gmail.com>
2024-09-28 20:16:09 +0530

    main removed mdconverter.js

--- src/array.js
+++ src/array.js
@@ -1,12 +1,12 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const newArray = arr.map((num) => {
-    return "x".repeat(num);
+	return "x".repeat(num);
 });
 for (let num = 0; num < newArray.length; num++) {
-    console.log(newArray[num]);
+	console.log(newArray[num]);
 
-    document.querySelector("#array").innerHTML = `
+	document.querySelector("#array").innerHTML = `
 <h1>${arr[num]}</h1>
 `;
 }
