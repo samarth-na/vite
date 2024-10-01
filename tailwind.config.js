462d7989db71f83f656fdbb7bed1f58c91bcd57c
Samarth Nagar <samarthxma2009@gmail.com>
2024-10-02 01:24:51 +0530

    main added github font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx,html}", "./htmls/*.html"],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx,html}",
+		"./htmls/*.html",
+	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				github: ["Inter", "sans-serif"], // Naming the font "github" for convenience
+			},
+		},
 	},
 	plugins: [],
 };
