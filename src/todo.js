7965224402505425559fb4534ffbac608db0e782
Samarth Nagar <samarthxma2009@gmail.com>
2024-11-03 05:01:24 +0530

    main backup

--- src/todo.js
+++ src/todo.js
@@ -2,8 +2,6 @@ import "../style.css";
 
 const todolist = document.querySelector("#list");
 
-const output = document.getElementById("output");
-
 const input = document.querySelector("#input");
 
 const addItem = document.getElementById("add");
@@ -11,7 +9,7 @@ const addItem = document.getElementById("add");
 const deleteItem = document.getElementById("delete");
 
 addItem.addEventListener("click", () => {
-	let listItem = document.createElement("li");
+	const listItem = document.createElement("li");
 	listItem.textContent = input.value;
 	todolist.appendChild(listItem);
 });
